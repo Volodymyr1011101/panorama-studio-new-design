64b1000f744eee32c395481d500f827d0283cc34
Volodymyr Voronkin <voronkinvladimir328@gmail.com>
2025-03-26 00:01:19 +0100

    fix

--- src/app/[locale]/(pages)/room/[room]/page.tsx
+++ src/app/[locale]/(pages)/room/[room]/page.tsx
@@ -3,8 +3,19 @@ import { storage } from '@/app/firebase';
 import { getImagesFromDataBase } from '@/helpers';
 import PageContent from '@/app/[locale]/(pages)/room/[room]/PageContent';
 
+export async function generateStaticParams() {
+    // Получение списка комнат из какого-то источника данных (например, Firebase или API)
+    const roomSlugs = ['white', 'art', 'aqua_dark', 'aqua_light']; // Это пример, замените на запрос данных
+
+    return roomSlugs.map(roomSlug => ({
+        room: roomSlug // Для каждой комнаты генерируем статичный путь
+    }));
+}
+
 const Room = async ({ params }: any) => {
-    const roomSlug = params.room; // получаем параметр слага
+    const roomSlug = (await params).room; // получаем параметр слага
+
+    // Запрос изображений для комнаты и оборудования
     const listRef = ref(storage, 'equipment');
     const equipmentImages = await getImagesFromDataBase(listRef);
     const galleryImageRef = ref(storage, roomSlug);
