c1182f8766ef349092d7b0fb07bcca81ee08bd33
Volodymyr Voronkin <voronkinvladimir328@gmail.com>
2025-07-03 16:51:28 +0200

    added dinamic import

--- src/app/[locale]/(pages)/page.tsx
+++ src/app/[locale]/(pages)/page.tsx
@@ -3,13 +3,8 @@ import Rooms from '@/app/components/Rooms/Rooms';
 import styles from './page.module.scss';
 import { images2 } from '@/app/[locale]/(pages)/mock_images';
 import Gallery from '@/app/components/ui/Gallery/Gallery';
-import { getInfo } from '@/helpers';
-import ImageSlider from '@/app/components/ui/Slider/Slider';
 
 export default async function HomePage() {
-    // const data = await getInfo();
-    // console.log(data);
-
     return (
         <div className={`px-4 pt-[102px] md:p-4 h-`}>
             <section className={styles.heroSection}>
@@ -18,9 +13,9 @@ export default async function HomePage() {
             <section className={styles.roomsSection}>
                 <Rooms />
             </section>
-            <section>
-                <ImageSlider />
-            </section>
+            {/*<section>*/}
+            {/*    <ImageSlider />*/}
+            {/*</section>*/}
             <section className=" mb-8">
                 <Gallery images={images2} header={'gallery_header'} />
             </section>
